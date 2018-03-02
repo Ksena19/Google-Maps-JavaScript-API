b5832c74ff45a269fbf01440fda13d057974244f
Oksana <oksanashukh@gmail.com>
2018-03-02 18:26:09 +0200

    google maps first push

---
+++ js/script.js
@@ -0,0 +1,8 @@
+  function initMap() {
+      
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 50.431782, lng: 30.516382},
+          zoom: 8
+        });
+      }
+
